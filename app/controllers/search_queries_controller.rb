52b5297bcef609d8a28a7e477856063602b01a18
devaleixo <gabriel.aleixo061@gmail.com>
2025-07-30 19:35:54 -0300

    logic to filter searches done

--- app/controllers/search_queries_controller.rb
+++ app/controllers/search_queries_controller.rb
@@ -4,10 +4,13 @@ class SearchQueriesController < ApplicationController
         ip = request.remote_ip
         query = params[:query]
         UserSearch.create(ip_address: ip, query: query)
+        UserSearch.clean_pyramid_queries_for(ip)
     end
     
     def new
+        @queries = UserSearch.group(:query).order('count_query desc').limit(5).count(:query)
     end
+
     def analitycs
     end    
 end
--- app/models/user_search.rb
+++ app/models/user_search.rb
@@ -2,6 +2,33 @@ class UserSearch < ApplicationRecord
     validates :ip_address, presence: true
     validates :query, presence: true
 
-    def filter_query
-    end
+    def self.clean_pyramid_queries_for(ip_address)
+        user_searches = where(ip_address: ip_address)
+                        .order(created_at: :desc)
+      
+        to_keep = []
+        processed_queries = Set.new
+      
+        user_searches.each do |search|
+          query = search.query.strip.downcase
+      
+          next if processed_queries.include?(query)
+      
+          is_redundant = to_keep.any? do |s|
+            s.query.strip.downcase.start_with?(query + ' ') &&
+            s.created_at > search.created_at
+          end
+      
+          unless is_redundant
+            to_keep << search
+            processed_queries << query
+          end
+        end
+      
+        # Exclui os registros que não fazem parte do conjunto "válido"
+        where(ip_address: ip_address)
+          .where.not(id: to_keep.map(&:id))
+          .destroy_all
+      end
+      
 end
